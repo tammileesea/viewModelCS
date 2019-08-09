e3b2a6ccb33af43d3c5048e9f927f18dcfb9a4e6
Tammi Lee <tammilee219@gmail.com>
2019-08-09 13:18:50 -0700

    4 routes showing different view Models/objects/strings/etc

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using viewModel.Models;
+
+namespace viewModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index(){
+            string myString = "WHAT is this NonSEnSe!!!!";
+            return View("Index", myString);
+        }
+
+        [HttpGet("numbers")]
+        public IActionResult Numbers(){
+            int[] numbs = new int[] {2, 4, 6, 8};
+            return View("Numbers", numbs);
+        }
+
+        [HttpGet("users")]
+        public IActionResult Users(){
+            string[] names = new string[]{
+                "sally", "billy", "joey", "moose"
+            };
+            return View("Users", names);
+        }
+
+        [HttpGet("user")]
+        public IActionResult User(){
+            User user1 = new User(){
+                FirstName = "Tam",
+                LastName = "Lee"
+            };
+            return View("User", user1);
+        }
+    }
+}
